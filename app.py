f12611c2c289afe5de106dc6b6c6f502d95bca31
iahgo <iahgo@icloud.com>
2025-04-16 19:55:37 -0300

    acionando execucao do pipeline no jenkins

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Olá, DevOps! Minha aplicação Flask (infnet) está rodando no Kubernetes. Executando Pipeline..."
+    return "Olá, DevOps! Minha aplicação Flask (infnet) está rodando no Kubernetes. Executando Pipeline2..."
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
